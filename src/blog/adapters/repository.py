ecf36854daef256d5cab1776a09c913773051378
AlexH <alexh@getchip.uk>
2020-04-27 15:15:15 +0100

    add basic post, user and tests

--- src/blog/adapters/repository.py
+++ src/blog/adapters/repository.py
@@ -1,12 +1,13 @@
 import abc
-from blog.domain import Post
+from blog.domain.post import Post
+
 
 class AbstractRepository(abc.ABC):
 
     @abc.abstractmethod
-    def add(self, batch: model.Post):
+    def add(self, batch: Post):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def get(self, reference) -> model.Post:
+    def get(self, reference) -> Post:
         raise NotImplementedError
---
+++ src/blog/domain/user.py
@@ -0,0 +1,10 @@
+from datetime import date
+
+
+class User:
+    def __init__(self, user_id: str, first_name: str, last_name: str, role: str, created_at: date):
+        self.id = user_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.role = role
+        self.created_at = created_at
